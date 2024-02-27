8260ae81be8537f9171b81acd4c0167d29b451af
Tamunaut <112294830+Tamunaut@users.noreply.github.com>
2024-02-27 12:08:27 -0600

    Started integrating OrderSummary into PaymentPage.

--- GUI/PaymentPage.java
+++ GUI/PaymentPage.java
@@ -53,10 +53,12 @@ public class PaymentPage extends JPanel {
         loadMiddlePanel();
         add(middlePanel, BorderLayout.CENTER);
 
-        JPanel orderSummary = new JPanel(new FlowLayout());
+        JPanel orderSummary = new OrderSummary();
+        /*
         orderSummary.setBackground(Color.MAGENTA);
         orderSummary.setPreferredSize(new Dimension(400, 800));
         add(orderSummary, BorderLayout.EAST);
+        */
     }
 
     private void loadMiddlePanel() {

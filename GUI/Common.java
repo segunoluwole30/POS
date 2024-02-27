b4dc06bd691764f3747fea71e756f16484df3d11
segunoluwole30 <segunoluwole30@gmail.com>
2024-02-26 23:30:07 -0600

    added dark cyan to common

--- GUI/Common.java
+++ GUI/Common.java
@@ -1,7 +1,8 @@
 import java.awt.*;
 
 public class Common {
-        public static final Color MAROON = new Color(78, 18, 26);
-        public static final int WIDTH = 1600;
-        public static final int HEIGHT = 900;
-    }
+    public static final Color MAROON = new Color(78, 18, 26);
+    public static final Color DARKCYAN = new Color(18, 78, 70);
+    public static final int WIDTH = 1600;
+    public static final int HEIGHT = 900;
+}

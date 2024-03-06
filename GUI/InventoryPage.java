34c808361c2dbcc6b24f43958b8ff9e252923439
Kalclavia <crrzyman@gmail.com>
2024-03-06 00:07:30 -0600

    Fixed issue with inventory items not deleted

--- GUI/InventoryPage.java
+++ GUI/InventoryPage.java
@@ -18,7 +18,7 @@ public class InventoryPage extends JPanel {
     // UI Elements
     private JPanel navbar;
     private JPanel mainPanel;
-    private JTable inventoryTableDisplay;
+    private JTable inventoryTable;
     private JTable suggestionsTable;
     private JButton addButton, deleteButton;
     private DefaultTableModel tableModel;
@@ -30,17 +30,13 @@ public class InventoryPage extends JPanel {
     private String InventoryQuery = "SELECT * FROM ingredientsinventory ORDER BY ingredientid;";
     private String SuggestionQuery = "";
 
-    private SmartTable inventoryTable;
-
     // Constructor
     public InventoryPage(Connection conn, POS pos) {
         this.conn = conn;
         this.pos = pos;
-        inventoryTable = new SmartTable(conn, InventoryQuery);
-        inventoryTable.refreshTableData();
         initializeUI();
+        refreshTableData(InventoryQuery);
         repaint();
-
     }
 
     private void initializeUI() {
@@ -95,14 +91,46 @@ public class InventoryPage extends JPanel {
         inventoryTitle.setOpaque(false);
         inventoryTitle.setEditable(false);
 
-        inventoryTableDisplay = new JTable(inventoryTable.tableModel);
+        tableModel = new DefaultTableModel(new String[] { "Item ID", "Name", "Stock", "MaxStock", "Units" }, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return true;
+            }
+        };
 
-        inventoryTableDisplay.setRowHeight(Common.HEIGHT / 16);
-        inventoryTableDisplay.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        inventoryTableDisplay.getTableHeader().setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        inventoryTable = new JTable(tableModel);
+        inventoryTable.setModel(tableModel);
+
+        // Listen to cell edits
+        tableModel.addTableModelListener(e -> {
+            if (e.getType() == TableModelEvent.UPDATE) {
+                int row = e.getFirstRow();
+                int column = e.getColumn();
+                Object id = tableModel.getValueAt(row, 0);
+
+                if (id == null || id.toString().isEmpty()) {
+                    // This is a new row, handle the insert operation
+                    String name = tableModel.getValueAt(row, 1).toString();
+                    float stock = Float.parseFloat(tableModel.getValueAt(row, 2).toString());
+                    float maxstock = Float.parseFloat(tableModel.getValueAt(row, 3).toString());
+                    String units = tableModel.getValueAt(row, 4).toString();
+                    insertNewItem(name, stock, maxstock, units);
+                    refreshTableData(InventoryQuery);
+
+                } else {
+                    // Existing row, handle the update operation
+                    Object value = tableModel.getValueAt(row, column);
+                    updateMenuItemInDatabase(id, column, value);
+                }
+            }
+        });
+
+        inventoryTable.setRowHeight(Common.HEIGHT / 16);
+        inventoryTable.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        inventoryTable.getTableHeader().setFont(new Font("Times New Roman", Font.PLAIN, 16));
 
         // > scroll pane for table
-        JScrollPane InventoryScrollPane = new JScrollPane(inventoryTableDisplay);
+        JScrollPane InventoryScrollPane = new JScrollPane(inventoryTable);
         InventoryScrollPane
                 .setPreferredSize(new Dimension(InventoryScrollPane.getPreferredSize().width, Common.HEIGHT / 4));
 
@@ -116,21 +144,28 @@ public class InventoryPage extends JPanel {
         gbc.gridy++;
         inventoryPanel.add(InventoryScrollPane, gbc);
 
+        inventoryTable.getSelectionModel().addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                boolean rowSelected = inventoryTable.getSelectedRow() >= 0;
+                // editIngredientsButton.setEnabled(rowSelected);
+            }
+        });
+
         JButton addButton = new JButton("Add New Row");
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                inventoryTable.tableModel.addRow(new Object[] { "", "", "", "", "" }); // Adjust based on your data structure
+                tableModel.addRow(new Object[] { "", "", "", "", "" }); // Adjust based on your data structure
             }
         });
 
         JButton deleteButton = new JButton("Delete Selected Item");
         deleteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int selectedRow = inventoryTable.table.getSelectedRow();
+                int selectedRow = inventoryTable.getSelectedRow();
                 if (selectedRow != -1) {
                     Object id = tableModel.getValueAt(selectedRow, 0); // Assuming first column is ID
-                    inventoryTable.tableModel.removeRow(selectedRow);
-                    inventoryTable.deleteItemFromDatabase(id);
+                    tableModel.removeRow(selectedRow);
+                    deleteItemFromDatabase(id);
                     // Add code to delete the row from the database using `id`
                 }
             }
@@ -159,13 +194,14 @@ public class InventoryPage extends JPanel {
         suggestionsTitle.setOpaque(false);
         suggestionsTitle.setEditable(false);
         // > get table data
-        tableData = requestInventoryTable("SELECT * FROM ingredientsInventory WHERE stock / maxstock < 0.6 ORDER BY stock / maxstock ASC;");
+        tableData = requestInventoryTable(
+                "SELECT * FROM ingredientsInventory WHERE stock / maxstock < 0.6 ORDER BY stock / maxstock ASC;");
         String[][] rowEntries = new String[tableData.size()][];
         String[] columnEntries = { "Ingredient ID", "Name", "Current Stock", "Max Stock", "Units" };
         for (int i = 0; i < tableData.size(); i++) {
             rowEntries[i] = tableData.get(i);
         }
-        
+
         // > table
         suggestionsTable = new JTable(rowEntries, columnEntries);
         suggestionsTable.setEnabled(false);
@@ -196,9 +232,18 @@ public class InventoryPage extends JPanel {
                             + ");";
                     Statement stmt = conn.createStatement();
                     stmt.executeUpdate(restockQuery);
-                    suggestionsTable.repaint();
-                    inventoryTable.refreshTableData();
-                    inventoryTable.table.repaint();
+
+                    tableData = requestInventoryTable(
+                            "SELECT * FROM ingredientsInventory WHERE stock / maxstock < 0.6 ORDER BY stock / maxstock ASC;");
+                    String[][] rowEntries = new String[tableData.size()][];
+                    String[] columnEntries = { "Ingredient ID", "Name", "Current Stock", "Max Stock", "Units" };
+                    for (int i = 0; i < tableData.size(); i++) {
+                        rowEntries[i] = tableData.get(i);
+                    }
+                    suggestionsTable = new JTable(rowEntries, columnEntries);
+
+                    refreshTableData(InventoryQuery);
+                    inventoryTable.repaint();
                 } catch (Exception ee) {
                     JOptionPane.showMessageDialog(null, "Error accessing Database.");
                 }
@@ -261,6 +306,147 @@ public class InventoryPage extends JPanel {
             return gbl.getConstraints(component);
         } else {
             return null;
+        }
+    }
+
+    private void insertNewItem(String name, float stock, float maxstock, String units) {
+        String sql = "INSERT INTO IngredientsInventory (Name, Stock, MaxStock, Units) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1, name);
+            pstmt.setFloat(2, stock);
+            pstmt.setFloat(3, maxstock);
+            pstmt.setString(4, units);
+
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Creating item failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    ItemIDs.add(generatedKeys.getInt(1)); // Store the new MenuItemID
+                } else {
+                    throw new SQLException("Creating item failed, no ID obtained.");
+                }
+            }
+            JOptionPane.showMessageDialog(this, "Item added successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Database error: " + e.getMessage(), "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void deleteItem() {
+        int selectedRow = inventoryTable.getSelectedRow();
+        if (selectedRow >= 0) {
+            int confirmation = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete this item?",
+                    "Confirm Deletion", JOptionPane.YES_NO_OPTION);
+            if (confirmation == JOptionPane.YES_OPTION) {
+                int DelID = ItemIDs.get(selectedRow); // Get the ID from the list
+                deleteItemFromDatabase(DelID);
+                refreshTableData(InventoryQuery);
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Please select an item to delete.");
+        }
+    }
+
+    private void deleteItemFromDatabase(Object itemId) {
+        String sql = "DELETE FROM IngredientsInventory WHERE IngredientID = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, (Integer) itemId);
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows > 0) {
+                JOptionPane.showMessageDialog(null, "Item deleted successfully.");
+            } else {
+                JOptionPane.showMessageDialog(null, "No item was deleted.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error deleting item: " + e.getMessage(), "Database Error",
+                    JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+    }
+
+    private void updateMenuItemInDatabase(Object id, int column, Object value) {
+        String columnName;
+        String sql;
+        switch (column) {
+            case 1:
+                columnName = "Name";
+                sql = "UPDATE IngredientsInventory SET " + columnName + " = ? WHERE IngredientID = ?";
+                break;
+            case 2:
+                columnName = "Stock";
+                // Explicitly casting the parameter to double precision in PostgreSQL
+                sql = "UPDATE IngredientsInventory SET " + columnName
+                        + " = CAST(? AS double precision) WHERE IngredientID = ?";
+                break;
+            case 3:
+                columnName = "MaxStock";
+                sql = "UPDATE IngredientsInventory SET " + columnName
+                        + " = CAST(? AS double precision) WHERE IngredientID = ?";
+                break;
+            case 4:
+                columnName = "Units";
+                sql = "UPDATE IngredientsInventory SET " + columnName + " = ? WHERE IngredientID = ?";
+                break;
+
+            default:
+                throw new IllegalArgumentException("Invalid column index");
+        }
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            if (column == 2) { // For "Stock" column
+                // Use setObject with explicit parsing for the Stock column
+                float priceValue = Float.parseFloat(value.toString());
+                pstmt.setObject(1, priceValue);
+
+            } else if (column == 3) { // For "MaxStock" column
+                // Use setObject with explicit parsing for the MaxStock column
+                float priceValue = Float.parseFloat(value.toString());
+                pstmt.setObject(1, priceValue);
+
+            } else {
+                pstmt.setObject(1, value); // For other columns
+            }
+            pstmt.setInt(2, (Integer) id);
+
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Update was successful for IngredientID " + id + " for column " + columnName
+                        + " with new value: " + value);
+            } else {
+                System.out.println("No rows affected. Possible error in update operation for MenuItemID " + id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Error updating database for IngredientID " + id + ": " + e.getMessage(),
+                    "Database Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void refreshTableData(String query) {
+        tableModel.setRowCount(0); // Clear existing data
+        ItemIDs.clear();
+
+        try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                int id = rs.getInt("IngredientID");
+                String name = rs.getString("Name");
+                float stock = rs.getFloat("Stock");
+                float maxstock = rs.getFloat("MaxStock");
+                String units = rs.getString("Units");
+
+                ItemIDs.add(id);
+                tableModel.addRow(new Object[] { id, name, stock, maxstock, units });
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error fetching menu items: " + e.getMessage(), "Database Error",
+                    JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
     }
 }
